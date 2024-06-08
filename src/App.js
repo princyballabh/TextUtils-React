9e5a02a615e13ff05343a4335af1f4499381aba7
Princy Ballabh <princyballabh13@gmail.com>
2024-06-09 00:03:40 +0530

    first commit

--- src/App.js
+++ src/App.js
@@ -1,24 +1,63 @@
-import logo from './logo.svg';
-import './App.css';
 
+import './App.css';
+import Navbar from './components/Navbar';
+import Textform from './components/Textform';
+import Alert from './components/Alert';
+// import About from './components/About';
+import React, {useState} from 'react';
+// import { BrowserRouter, Route, Routes } from "react-router-dom";
+
+// import About from './components/About';
 function App() {
+  const [mode,setmode]=useState('light');
+  const [text,settext]=useState('Enable Dark Mode');
+  const [alert,setalert]=useState(null);
+
+  const showalert=(message,type)=>{
+    setalert({
+      msg: message,
+      type: type
+    })
+    setTimeout(() => {
+      setalert(null);
+    }, 1500);
+  }
+
+
+
+
+  const togglemode=()=>{
+    if(mode==='light'){
+      setmode('dark');
+      settext("Enable Light Mode")
+      showalert("Dark Mode Enabled","success")
+      document.body.style.backgroundColor='#04072b';
+    }
+    else{
+    setmode('light');
+    settext("Enable Dark Mode")
+    showalert("Light Mode Enabled","success")
+    document.body.style.backgroundColor= 'white';
+    }
+  }
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+   <>
+    {/* <BrowserRouter> */}
+    <Navbar title="TextUtils" mode={mode} text={text} togglemode={togglemode} about="About"/>
+   <Alert alert={alert}/>
+   <div className="container my-3">
+   {/* <Routes>
+      <Route path="/about" element={<About />} />
+    </Routes>
+    <Routes>
+      <Route
+        path="/"
+        element={ */}
+        <Textform heading="Enter the text to analyze" mode={mode}/>
+    {/* </Routes>   */}
+   </div> 
+   {/* </BrowserRouter> */}
+   </>
   );
 }
 
---
+++ src/components/Textform.js
@@ -0,0 +1,56 @@
+import React, {useState} from 'react'
+export default function Textform(props) {
+  const handleupclick= ()=>{
+    let newtext=text.toUpperCase();
+    setText(newtext)
+  }
+
+  const handleloclick= ()=>{
+    let newtext=text.toLowerCase();
+    setText(newtext)
+  }
+
+  const handleclrclick= ()=>{
+    let newtext='';
+    setText(newtext)
+  }
+
+  const handlespaces= ()=>{
+    let newText=text.split(/[ ]+/);
+    setText(newText.join(" "))
+  }
+
+  const handlecopyclick= ()=>{
+    var text=document.getElementById("myBox");
+    text.select();
+    navigator.clipboard.writeText(text.value);
+  }
+  
+  const handleonchange=(event)=>{
+    setText(event.target.value)
+  }
+
+  const [text, setText]=useState("");
+  return (
+    <>
+        <div className="container" style={{color:props.mode==='light'?'black':'white'}}>
+            <div className="mb-3" >
+                <h4>{props.heading}</h4>
+                <textarea className="form-control" value={text} style={{backgroundColor:props.mode==='dark'?'#04072b':'white', color:props.mode==='light'?'black':'white'}} onChange={handleonchange} id="myBox" rows="6"></textarea>
+            </div>
+            <button className="btn btn-primary mx-3" onClick={handleupclick}>Convert to Uppercase</button>
+            <button className="btn btn-primary mx-3" onClick={handleloclick}>Convert to Lowercase</button>
+            <button className="btn btn-primary mx-3" onClick={handleclrclick}>Clear Text</button>
+            <button className="btn btn-primary mx-3" onClick={handlecopyclick}>Copy Text</button>
+            <button className="btn btn-primary mx-3" onClick={handlespaces}>Remove Extra Spaces</button>
+        </div>
+        <div className="container my-2" style={{color:props.mode==='light'?'black':'white'}}>
+          <h4>Your Text Summary</h4>
+          <p>Words: {text.split(' ').filter((element)=>{return element.length!==0}).length} and Characters: {text.length}</p>
+          <p>Minutes read: {0.008*text.split(' ').length}</p>
+          <h6>Preview</h6>
+          <p>{text}</p>
+        </div>
+      </>
+  )
+}
